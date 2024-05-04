53afffe405d31686a78628becb94a024c4624fba
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-04 20:15:46 +0500

    papka

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { useLoaderData,Link } from "react-router-dom"
 import {formatPrice} from "../utils"
 
 function ProductsGrid() {
@@ -10,13 +10,14 @@ function ProductsGrid() {
             const {title,price,image}=product.attributes;
             const dollarAmount =formatPrice(price);
             return(
-                <Link className="card w-full shadow hover:shadow-2xl transition duration-300"
+                <Link className="card w-full shadow hover:shadow-2xl transition duration-300 "
                   key={product.id}
-                  to={`/products${product.id}`}
+                  to={`/products/${product.id}`}
                 >
                  <figure className="px-4 pt-4">
                     <img className="rounded-xl h-64 md:h-48 w-full object-cover" 
-                    src={image} alt={title} />
+                    src={image} 
+                    alt={title} />
                  </figure>
                    <div className="card-body items-center text-center">
                       <h2 className="card-title capitalize tracking-wider">{title}</h2>
