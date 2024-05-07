fb83fbc36b67b45b017a15e6a71ac535d7991664
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-08 01:18:07 +0500

    papka

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,38 +1,34 @@
-import { useLoaderData, Link } from "react-router-dom";
-import { formatPrice } from "../utils";
+import React from 'react'
 
-function ProductsList() {
-  const { products } = useLoaderData();
+import { Filters, ProductsContainer } from "../components"
+import { customFetch } from "../utils";
+
+const url = "/products";
+
+export const loader = async ({ request }) => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  const meta = response.data.meta;
+  console.log(products , meta);
+  return { products , meta , }
+};
+
+
+function Products() {
   return (
-    <div className="mt-12 grid gap-y-8">
-      {products.map((product) => {
-        const { title, image, company, price } = product.attributes;
-        const dollarAmount = formatPrice(price);
-        return (
-          <Link
-            className="p-8 rounded-lg flex flex-col sm:flex-row gap-y-4 flex-wrap bg-base-100 shadow-xl hover:shadow-2xl"
-            to={`/products/${product.id}`}
-            key={product.id}
-          >
-            <img
-              src={image}
-              alt={title}
-              className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover group-hover:scale-105 transition duration-300"
-            />
-            <div className="ml-0 sm:ml-16">
-              <h3 className="capitalize font-medium text-lg">{title}</h3>
-              <h4 className="capitalize text-md text-neutral-content">
-                {company}
-              </h4>
-            </div>
-            <p className="font-medium ml-0 sm:ml-auto text-lg">
-              {dollarAmount}
-            </p>
-          </Link>
-        );
-      })}
-    </div>
-  );
-}
+    <div>
+      <h1>Product</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, accusamus!</p>
+    <div className="align-content mt-14">
+      {/* FILTER*/}
+      <Filters/>
 
-export default ProductsList;
+      {/* ProductsContainer*/}
+      <ProductsContainer/>
+
+      {/* PaginationContainer*/}
+
+    </div>
+  )
+}
+export default Products
