a6fcb248e777bc50aea2a385eafa78081734de69
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-11 19:07:44 +0500

    papka

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -9,7 +9,7 @@ import { addItem } from "../features/cart/cartSlice"
 
 
 export const loader = async (params) => {
-  const request = await customFetch(`/products/${params.id}`);
+  const request = await customFetch(`/products/${params.params.id}`);
   return {product: request.data.data};
 }
 
@@ -37,7 +37,7 @@ function SingleProduct() {
     dispatch(addItem({product: cartProduct}));
   }
   return (
-    <section className="align-content">
+    <section className="align-content py-20">
       <div className="text-md breadcrumbs">
         <ul>
           <li>
@@ -53,12 +53,12 @@ function SingleProduct() {
         <img className="w-96 h-96 object-cover rounded-lg" src={image} alt={title} />
         <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
-          <h4 className="title-xl font-bold text-netural-content mt-2">{company}</h4>
+          <h4 className="text-xl font-bold text-neutral-content mt-2">{company}</h4>
           <p className="mt-3 text-xl">{dollarAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
           <div className="mt-2">
             {colors.map((color)=>{
-              return <button key={color} className={`badge w06 h-6 mr-2 ${color == productColor && "border-2 border-secondary"}`} style={{backgroundColor: color}} onClick={() => setProductColor(color)}></button>
+              return <button key={color} className={`badge w-6 h-6 mr-2 ${color == productColor && "border-2 border-secondary"}`} style={{backgroundColor: color}} onClick={() => setProductColor(color)}></button>
             })}
           </div>
           <div className="form-control w-full max-w-xs">
@@ -67,7 +67,7 @@ function SingleProduct() {
             </label>
             <select className="select select-secondary select-bordered select-md" value={amount} onChange={(e) => setAmount(e.target.value)}>{generateAmountOptions(20)}</select>
           </div>
-          <div className="mt-10"><button onClick={() =>console.log("add to bag")} className="btn btn-secondary btn-md">Add to bag</button></div>
+          {/* <div className="mt-10"><button onClick={() =>console.log("add to bag")} className="btn btn-secondary btn-md">Add to bag</button></div> */}
           <div className="mt-10"><button onClick={addToCart} className="btn btn-secondary btn-md">Add to bag</button></div>
         </div>
       </div>
